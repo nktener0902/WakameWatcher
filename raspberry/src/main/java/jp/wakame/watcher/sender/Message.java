acb4f0f25b5c2d967f0cb8c1607e6e18a01b4a73
Yu Nakata <y-nakata@outlook.com>
2017-11-21 22:26:44 +0900

    [REMOVE] 不要なファイルを削除

--- raspberry/src/main/java/jp/wakame/watcher/sender/Message.java
+++ raspberry/src/main/java/jp/wakame/watcher/sender/Message.java
@@ -1,5 +1,9 @@
 package jp.wakame.watcher.sender;
 
+import java.util.logging.Logger;
+
+import javax.inject.Inject;
+
 import com.amazonaws.services.iot.client.AWSIotMessage;
 import com.amazonaws.services.iot.client.AWSIotQos;
 
@@ -8,18 +12,24 @@ public class Message extends AWSIotMessage {
         super(topic, qos, payload);
     }
 
+	@Inject
+	transient Logger log;
+
     @Override
     public void onSuccess() {
         // called when message publishing succeeded
+    	log.info(System.currentTimeMillis() + ": >>> " + getStringPayload());
     }
 
     @Override
     public void onFailure() {
         // called when message publishing failed
+    	log.severe(System.currentTimeMillis() + ": publish failed for " + getStringPayload());
     }
 
     @Override
     public void onTimeout() {
         // called when message publishing timed out
+    	log.severe(System.currentTimeMillis() + ": publish timeout for " + getStringPayload());
     }
 }
