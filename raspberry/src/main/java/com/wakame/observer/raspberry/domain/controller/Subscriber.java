0f806a9ccc353d86b0e57f94b3734bc9afb62423
Yu Nakata <y-nakata@outlook.com>
2020-01-01 19:08:47 +0900

    Writing sqs client

--- raspberry/src/main/java/com/wakame/observer/raspberry/domain/controller/Subscriber.java
+++ raspberry/src/main/java/com/wakame/observer/raspberry/domain/controller/Subscriber.java
@@ -12,28 +12,29 @@ import java.util.List;
 @Controller
 public class Subscriber {
 
-    String queueName = "queue" + System.currentTimeMillis();
-    SqsClient sqsClient = SqsClient.builder().region(Region.US_WEST_2).build();
+    public PhotoOrderMessage incoming(){
 
-    // snippet-start:[sqs.java2.sqs_example.create_queue]
-    CreateQueueRequest createQueueRequest = CreateQueueRequest.builder().queueName(queueName).build();
+        String queueName = "queue" + System.currentTimeMillis();
+        SqsClient sqsClient = SqsClient.builder().region(Region.US_WEST_2).build();
+
+        // snippet-start:[sqs.java2.sqs_example.create_queue]
+        CreateQueueRequest createQueueRequest = CreateQueueRequest.builder().queueName(queueName).build();
         sqsClient.createQueue(createQueueRequest);
-    // snippet-end:[sqs.java2.sqs_example.create_queue]
+        // snippet-end:[sqs.java2.sqs_example.create_queue]
 
         System.out.println("\nGet queue url");
-    // snippet-start:[sqs.java2.sqs_example.get_queue]
-    GetQueueUrlResponse getQueueUrlResponse =
-            sqsClient.getQueueUrl(GetQueueUrlRequest.builder().queueName(queueName).build());
-    String queueUrl = getQueueUrlResponse.queueUrl();
+        // snippet-start:[sqs.java2.sqs_example.get_queue]
+        GetQueueUrlResponse getQueueUrlResponse =
+                sqsClient.getQueueUrl(GetQueueUrlRequest.builder().queueName(queueName).build());
+        String queueUrl = getQueueUrlResponse.queueUrl();
         System.out.println(queueUrl);
-    // snippet-end:[sqs.java2.sqs_example.get_queue]
-
-    public PhotoOrderMessage incoming(){
+        // snippet-end:[sqs.java2.sqs_example.get_queue]
         ReceiveMessageRequest receiveMessageRequest = ReceiveMessageRequest.builder()
                 .queueUrl(queueUrl)
                 .maxNumberOfMessages(5)
                 .build();
         List<Message> messages= sqsClient.receiveMessage(receiveMessageRequest).messages();
+
         return new PhotoOrderMessage();
     }
 }
